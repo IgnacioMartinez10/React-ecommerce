7aa002fea52f794480f93780aab063d16888ed9c
Ignacio Martinez <ignaciomartinez923@gmail.com>
2022-05-16 18:38:49 -0300

    agregando navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import logo from './logo.svg';
+import NavBar from './components/NavBar/NavBar';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar/>
+        <img  src={logo} className="App-logo" alt="logo"/>
+        <h1 style={{color: 'white'}}>
+          Hola
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import './NavBar.css'
+const NavBar = () => {
+    return (
+        <nav className='nav'>
+            <div className='contentButton'>
+                <button>Inicio</button>
+                <button>Productos</button>
+                <button>Nosotros</button>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
